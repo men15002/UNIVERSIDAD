8ea48b5b81a7dde38349415984247b901bdd3302
Carlos <carlos.calderon097@gmail.com>
2016-07-09 12:14:32 -0600

    Version 2.0. Programa casi terminado, solo falta corregir la parte de guardado en botones.

---
+++ ADT/src/MainIPOD.java
@@ -0,0 +1,26 @@
+import java.awt.EventQueue;
+/**
+Clase driver para ejecutar el programa
+ * 	@author Carlos Calderon, 15219
+ *  @author Jorge Azmitia,15202
+ * 	@version 2.0.0 Julio 9, 2016
+*/
+public class MainIPOD {
+	/**
+	 * Ejecutar aplicacion
+	 */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							GUI_IPOd frame = new GUI_IPOd();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+	}
+}
